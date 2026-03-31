26494b82d044d42f749fd291cb8ae674fa8e8b1f
Baki Toplu <11xybaki@gmail.com>
2026-03-31 17:05:55 +0300

    Update

---
+++ bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using bookDemo.Models;
+
+namespace bookDemo.Controllers
+{
+    [Route("api/Books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}
---
+++ bookDemo/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers(); 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Logging.ClearProviders(); // Mevcut tüm log sağlayıcılarını temizleyin
+builder.Logging.AddConsole(); // Konsol log sağlayıcısını ekleyin
+builder.Logging.AddDebug(); // Debug log sağlayıcısını ekleyin
+
+var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+// app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
