2eb1a0edc0f0925b175dbd2e41982b07bb54251c
Baki Toplu <11xybaki@gmail.com>
2026-05-11 10:40:14 +0300

    Update

--- bsStoreApp/Services/obj/Debug/net10.0/Services.AssemblyInfo.cs
+++ bsStoreApp/Services/obj/Debug/net10.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa8ea8768a22efa7531522ea550d47f97d8f9f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+790d145a9d6cb924dadd6313243332296757bcad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
